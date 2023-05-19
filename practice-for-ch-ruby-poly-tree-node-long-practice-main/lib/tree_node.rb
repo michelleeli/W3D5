29b260003738afa8ec1ec13df7dc71937a1def0f
Viktoria Czaran <vczaran@gmail.com>
2023-05-19 14:24:31 -0400

    Working on #parent=

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -9,10 +9,30 @@ class PolyTreeNode
     end 
 
     def parent= (node)
-        @parent = node
-        if !parent.children.include?(self)
-             parent.children << self
-        end 
+        if node != nil
+          @parent = node
+          parent.children << self unless parent.children.include?(self)
+        end
+
+        return nil
+       
+        # if self.parent != nil && self.parent != node
+        #     self.parent.children.delete(self)   
+        #     @parent = node 
+        # end
+
+        #     if self.parent != nil && !parent.children.include?(self)
+        #         @parent = node
+        #         parent.children << self
+        #    end 
+
     end 
+
+    def inspect
+        "#PolyTreeNode#{self.object_id}  @parent=#{@parent}   @children = #{@children}"
+    end
+
+
+
 
 end
